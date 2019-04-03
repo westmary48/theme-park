a256558011e1d39d92156cd48ae285dd2f4fc3fa
Mary West <westmarye1219@gmail.com>
2019-04-02 21:18:59 -0500

    wip

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
-console.log('hi');
+// xhr XmlHttpREQUEST
+let rides = [];
+
+const domStringBuilder = (arrayToPrint) => {
+    console.log(arrayToPrint);
+}
+
+function executeThisCodeAfterFileLoads(){
+  const data = JSON.parse(this.responseText);
+  rides = data.rides;
+  domStringBuilder(data.rides);
+
+}
+
+function executeThisCodeIfXHRFails() {
+    console.error('oh shit');
+}
+
+const getRidesData = () => {
+    // constructor XMLHttpRequest();
+  const myRequest = new XMLHttpRequest();
+  myRequest.addEventListener('load', executeThisCodeAfterFileLoads);
+  // My request is a copy of XMLHttpRequest();
+  myRequest.addEventListener('error', executeThisCodeIfXHRFails);
+  myRequest.open('GET', './db/rides.json');
+  myRequest.send();
+    console.log(myRequest);
+
+}
+
+// constructor - predetermined object that you can modify later
+
+
+const init = () => {
+    getRidesData();
+}
+
+init();
